386f0f2b8f8450509415b53f6f1e82bf410475a4
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-06-06 20:29:37 +0600

    Creating a custom API for additional loading of posts

---
+++ pages/api/outsideReq.ts
@@ -0,0 +1,22 @@
+export default async function outsideReq(req: any, res: any) {
+    const response = await fetch(`https://zenquotes.io/api/quotes`)
+    let aforisms = await response.json()
+
+    aforisms.forEach(async function (item: any) {
+        const response = await fetch(`${process.env.HOST}:4200/aforisms`, {
+            method: 'POST', headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }, body: JSON.stringify(
+                {
+                    words: item.q,
+                    author: item.a,
+                    likes: 0
+                })
+            
+        });
+
+        await response.json()
+    })
+
+    res.status(200).json({})
+}
