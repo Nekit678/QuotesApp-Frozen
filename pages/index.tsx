f49653b5c1d25ab1f1942ecd60e32e48401e6ef0
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-06-10 05:08:25 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,9 @@
-import { useState } from 'react'
-import Block from '../components/Block'
-import styles from '../styles/Home.module.css';
+import { useRouter } from 'next/router';
 
-export default function Home() {
-
-  const [list, setList] = useState(Array())
-  const [count, setCount] = useState(0)
-
-  if (count == 0 && typeof window !== "undefined") {
-    showPosts()
+export default function () {
+  const router = useRouter()
+  if (typeof window !== "undefined") {
+    router.push('/posts/1')
   }
-
-  async function showPosts() {
-    setCount(count + 5)
-
-
-    if (JSON.parse(String(localStorage.getItem("likes"))) == null) {
-      localStorage.setItem("likes", "[]")
-    }
-    const data = JSON.parse(String(localStorage.getItem("likes")))
-
-    const response = await fetch(`api/load`, {
-      method: 'POST', headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      }, body: JSON.stringify(
-        {
-          start_id: count
-        })
-    });
-
-    const aforisms = await response.json()
-
-    if (aforisms.length < 5) {
-      const response = await fetch(`api/outsideReq`)
-
-      setTimeout(async function () {
-        const response = await fetch(`api/load`, {
-          method: 'POST', headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-          }, body: JSON.stringify(
-            {
-              start_id: count
-            })
-        });
-        const aforisms = await response.json()
-        setList([...list, (<div key={count}><Block aforisms={aforisms} data={data} /></div>)])
-      }, 200)
-      
-    }
-    else {
-      setList([...list, (<div key={count}><Block aforisms={aforisms} data={data} /></div>)])
-    }
-
-  }
-
-  return (
-    <div className={styles.background}>
-      <h1 className={styles.head}>Афоризмы</h1>
-      {list}
-      <input type="button" value="Смотреть дальше" className={styles.button} onClick={showPosts}></input>
-    </div>
-  )
 }
+
